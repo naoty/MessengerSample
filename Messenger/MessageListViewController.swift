6e14cb62a8ac2bcabdd9af2f7da37288aeba4ca6
Naoto Kaneko <naoty.k@gmail.com>
2014-11-11 18:21:38 +0900

    Initial commit

---
+++ Messenger/MessageListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MessageListViewController.swift
+//  Messenger
+//
+//  Created by 金子 直人 on 2014/11/11.
+//  Copyright (c) 2014年 Naoto Kaneko. All rights reserved.
+//
+
+import UIKit
+
+class MessageListViewController: SLKTextViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Messenger/Messenger-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Messenger-Bridging-Header.h
+//  Messenger
+//
+//  Created by 金子 直人 on 2014/11/11.
+//  Copyright (c) 2014年 Naoto Kaneko. All rights reserved.
+//
+
+#import "SLKTextViewController.h"
